9dfc62578141bbd3f69c1d84161d10f7e07e57a5
XInyi <yingmu52@gmail.com>
2016-12-09 15:45:23 -0800

    Update Documentations

--- LottoFinder/Brain.swift
+++ LottoFinder/Brain.swift
@@ -9,35 +9,13 @@
 import UIKit
 
 class Brain {
-    private(set) var configuration : [Int] = []
-    private(set) var string : String = ""
     
-    public init(string: String) {
-        self.string = string
-        
-        let len = string.characters.count
-        //        print("\(len) digits number\n\n")
-        let lowbound = 7
-        let upperbound = 14
-        if lowbound...upperbound ~= len {
-            let nSingleDigit = upperbound - len
-            let nDoubleDigit = len - lowbound
-            
-            if nDoubleDigit >= 1 {
-                for _ in 1...nDoubleDigit{
-                    self.configuration.append(2)
-                }
-            }
-            
-            if nSingleDigit >= 1 {
-                for _ in 1...nSingleDigit{
-                    self.configuration.append(1)
-                }
-            }
-        }
+    private(set) var string : String = ""
+    public init(str: String) {
+        self.string = str
     }
     
-    
+    //Untility function to generate a valid lotter tickets, cost is guarantee to be 0
     class func getRandomUniqueNumbers() -> [String] {
         var array = [String]()
         while array.count != 7 {
@@ -48,71 +26,90 @@ class Brain {
         }
         return array
     }
-
-    
 }
 
 extension Brain {
-    func anneal(inputConfig: [Int]) -> [Int] {
+    
+    /**
+     Simmulated Annealing Function, see https://en.wikipedia.org/wiki/Simulated_annealing
+     - important: Before calling this method, a Brain instance must be initialised with 'string' property set should be initialzed
+     - features:
+        1. A decreasing tempreture schdedule T
+        2. Evaluation of Local optimum using Brain.cost() function
+        3. The configuration is updated when it generates a lower cost ticket
+     - returns: a configuration array
+
+     */
+    func anneal() -> [Int]? {
         
-        var finalConfig = inputConfig
-        let finalStrings = map(configuration: finalConfig, string: string)
-        var finalCost = Brain.cost(luckyNumbers: finalStrings)
 
-
-        var T = 1.0
-        let T_min = 0.00001
-        let alpha = 0.9
-        while T > T_min {
-            var i = 1
-            while i <= 100 {
-                let newConfig = neighbour(config: finalConfig)
-                let newStrings = map(configuration: newConfig, string: string)
-                let newCost = Brain.cost(luckyNumbers: newStrings)
-                
-                if newCost == 0 {
-                    return newConfig
+        if let randomConfig = self.string.randomConfiguration() {
+            
+            //initialise configuration and evaluate its cost
+            var finalConfig = randomConfig
+            var finalCost = Brain.cost(numbers: self.string.luckyNumbers(for: randomConfig)!)
+            
+            var T = 1.0
+            let T_min = 0.00001
+            let alpha = 0.9
+            while T > T_min {
+                var i = 1
+                while i <= 100 {
+                    
+                    //get a neighbour config
+                    let tmpConfig = neighbour(config: finalConfig)
+                    let tmpCost = Brain.cost(numbers: self.string.luckyNumbers(for: tmpConfig)!)
+                    
+                    //a valid config is found
+                    if tmpCost == 0 {
+                        return tmpConfig
+                    }
+                    
+                    //move to the next state when accepted
+                    let accceptanceProbability = exp(Double(abs(tmpCost - finalCost)) / T)
+                    if accceptanceProbability > Double(arc4random_uniform(100)) / 100 {
+                        finalConfig = tmpConfig
+                        finalCost = tmpCost
+                    }
+                    i += 1
                 }
-                
-                let accceptanceProbability = exp(Double(abs(newCost - finalCost)) / T)
-                if accceptanceProbability > Double(arc4random_uniform(100)) / 100 {
-                    finalConfig = newConfig
-                    finalCost = newCost
-                }
-                i += 1
+                T *= alpha
             }
-            T *= alpha
-        }
-        return finalConfig
+            return finalConfig
 
-    }
-    
-    func map(configuration: [Int] ,string: String) -> [String] {
-        assert(configuration.reduce(0, +) == string.characters.count)
-        var results = [String]()
-        for i in 0 ..< configuration.count {
-            let currentPosition = results.joined(separator: "").characters.count
-            let s = string.substring(with: currentPosition ..< currentPosition + configuration[i])
-            results.append(s)
         }
-        return results
+        return nil
     }
     
     
     
+    /*
+     This function finds a neigbour configuration in Simmulated Annealing, 
+     it is basicaaly a configuration re-arrangement
+     */
     private func neighbour(config : [Int]) -> [Int] {
         var c = config
         c.shuffle()
         return c
     }
+
     
-    class func cost(luckyNumbers: [String]) -> Int {
-        /*
-         Note that a valid lottery ticket must have
-         1 - 7 unique numbers
-         2 - between 1 and 59,
-         3 - digits must be used in order, and every digit must be used.
-         */
+    
+    
+    /**
+     - parameters:
+        - numbers: a candidate array of numbers
+
+     Evaluate the cost of a candidate array of numbers
+     - Note that a valid lottery ticket must have
+        - 7 unique numbers
+        - between 1 and 59,
+        - digits must be used in order, and every digit must be used.
+     - the cost of a valid lottery ticket is 0, this function is used to help Simmulated Annealing method evaluate local optimum
+     
+     - returns: cost
+     */
+    class func cost(numbers: [String]) -> Int {
         
         let validLength = 7
         let lowerbound = 1
@@ -120,17 +117,19 @@ extension Brain {
         var score = 0 //when ever an invalid propertiy is found, 'score' goes up by 1
         
         //check valid length
-        if luckyNumbers.count != validLength {
+        if numbers.count != validLength {
             score += 1
         }
         
-        //check valid range
-        for s in luckyNumbers {
+        for s in numbers {
             
+            
+            //0 prefix is not accecpted, i.e "01"
             if s.hasPrefix("0"){
                 score += 1
             }
             
+            //check if each number is in 1~59
             if let s_int = Int(s) {
                 if s_int < lowerbound || s_int > upperbound {
                     score += 1
@@ -141,14 +140,95 @@ extension Brain {
         }
         
         //check if unique
-        score += abs(luckyNumbers.count - Array(Set(luckyNumbers)).count)
+        score += abs(numbers.count - Array(Set(numbers)).count)
         
         return score
     }
 
 }
 
+
 extension String {
+    
+    /* Valid Ticket Configuration
+     Number of digit       configuration      numbers of 1     numbers of 2
+     7                      1 1 1 1 1 1 1           14 - 7          7 - 7
+     8                      2 1 1 1 1 1 1           14 - 8          8 - 7
+     9                      2 2 1 1 1 1 1           14 - 9          9 - 7
+     10                     2 2 2 1 1 1 1           14 - 10         10 - 7
+     11                     2 2 2 2 1 1 1           14 - 11         11 - 7
+     12                     2 2 2 2 2 1 1           14 - 12         12 - 7
+     13                     2 2 2 2 2 2 1           14 - 13         13 - 7
+     14                     2 2 2 2 2 2 2           14 - 14         14 - 7
+     */
+    
+    func isValid(configuration: [Int]) -> Bool {
+        //as indicated above, the sum of configuration elements euqals the length of a valid ticket
+        return configuration.reduce(0, +) == self.characters.count
+    }
+
+    
+    func randomConfiguration() -> [Int]?{
+        //generate a random valid configuration with consists only 1 / 2
+        
+        let len = self.characters.count
+        
+        //valid ticket string must have length within 7 ~ 14 digits
+        let lowbound = 7
+        let upperbound = 14
+        
+        
+        if lowbound...upperbound ~= len {
+            var config = [Int]()
+            
+            let nSingleDigit = upperbound - len
+            let nDoubleDigit = len - lowbound
+            
+            
+            //append 2
+            if nDoubleDigit >= 1 {
+                for _ in 1...nDoubleDigit{
+                    config.append(2)
+                }
+            }
+            
+            //append 1
+            if nSingleDigit >= 1 {
+                for _ in 1...nSingleDigit{
+                    config.append(1)
+                }
+            }
+            return config
+        }
+        return nil
+    }
+
+    
+    /**
+     generate a lucky numbers based on configuration,
+     i.e.  "12345678" (string),  [1,1,1,1,1,1,2] (configuration) => ["1","2","3","4","5","6","78"]
+     - returns: An array of lucky numbers
+     */
+    func luckyNumbers(for configuration: [Int]) -> [String]? {
+        /* 
+         
+         */
+        if isValid(configuration: configuration) {
+            var results = [String]()
+            for i in 0 ..< configuration.count {
+                let currentPosition = results.joined(separator: "").characters.count
+                let s = self.substring(with: currentPosition ..< currentPosition + configuration[i])
+                results.append(s)
+            }
+            return results
+        }
+        return nil
+    }
+    
+}
+
+
+extension String { //ultility function to get substrings
     func index(from: Int) -> Index {
         return self.index(startIndex, offsetBy: from)
     }
@@ -156,11 +236,6 @@ extension String {
     func substring(from: Int) -> String {
         let fromIndex = index(from: from)
         return substring(from: fromIndex)
-    }
-    
-    func substring(to: Int) -> String {
-        let toIndex = index(from: to)
-        return substring(to: toIndex)
     }
     
     func substring(with r: Range<Int>) -> String {
@@ -184,13 +259,3 @@ extension MutableCollection where Indices.Iterator.Element == Index {
         }
     }
 }
-
-extension Sequence {
-    /// Returns an array with the contents of this sequence, shuffled.
-    func shuffled() -> [Iterator.Element] {
-        var result = Array(self)
-        result.shuffle()
-        return result
-    }
-}
-
--- LottoFinderTests/BrainTests.swift
+++ LottoFinderTests/BrainTests.swift
@@ -20,68 +20,41 @@ class BrainTests: XCTestCase {
         super.tearDown()
     }
     
-    func testBrain() {
-        for _ in 1...100 {
-            let uniqueNums = Brain.getRandomUniqueNumbers()
-            let inputString = uniqueNums.joined(separator: "")
-            let brain = Brain(string: inputString)
-            var sum = 0
-            for n in brain.configuration {
-                sum += n
-            }
-            print(uniqueNums)
-            print(brain.configuration)
-            assert(sum == inputString.characters.count)
-            assert(uniqueNums.count == brain.configuration.count)
-            
-
-            assert(Brain.cost(luckyNumbers: uniqueNums) == 0)
-         
-            
-            let mappedResult = brain.map(configuration: brain.configuration, string: inputString)
-            assert(mappedResult.count == brain.configuration.count)
-            
-            for i in 0 ..< mappedResult.count {
-                assert(mappedResult[i].characters.count == brain.configuration[i])
-            }
-            
-        }
-    }
     
     func testAnnealing(){
         
         //example test
         let in1 = "1234567"
-        let b1 = Brain(string: in1)
-        let out1 = b1.anneal(inputConfig: b1.configuration)
-        let cost1 = Brain.cost(luckyNumbers: b1.map(configuration: out1, string: in1))
+        let b1 = Brain(str: in1)
+        let out1 = b1.anneal()
+        let cost1 = Brain.cost(numbers:in1.luckyNumbers(for: out1!)!)
         
         let in2 = "4938532894754"
-        let b2 = Brain(string: in2)
-        let out2 = b2.anneal(inputConfig: b2.configuration)
-        let cost2 = Brain.cost(luckyNumbers: b2.map(configuration: out2, string: in2))
+        let b2 = Brain(str: in2)
+        let out2 = b2.anneal()
+        let cost2 = Brain.cost(numbers:in2.luckyNumbers(for: out2!)!)
         assert(cost1 == 0 && cost2 == 0)
-        
-        
+        print("\(in1) -> \(in1.luckyNumbers(for: out1!)!)")
+        print("\(in2) -> \(in2.luckyNumbers(for: out2!)!) \n")
         
         //pressure test
         var successCount = 0
-        let iterations = 10000
+        let iterations = 100000
         for _ in 1...iterations {
             let uniqueNums = Brain.getRandomUniqueNumbers()
             let inputString = uniqueNums.joined(separator: "")
-            let brain = Brain(string: inputString)
-            let outputConfiguration = brain.anneal(inputConfig: brain.configuration)
-            
-            let outputStrings = brain.map(configuration: outputConfiguration, string: inputString)
-            
-            if Brain.cost(luckyNumbers: outputStrings) == 0 {
-                print("Successed")
+            let brain = Brain(str: inputString)
+            let outputConfiguration = brain.anneal()
+            let outputStrings = inputString.luckyNumbers(for: outputConfiguration!)!
+
+            if Brain.cost(numbers: outputStrings) == 0 {
+//                print("Successed")
                 successCount += 1
             }else{
-                print("Failed")
+//                print("Failed")
             }
-//            print("\(inputString) -> \(outputStrings)")
+//            print("\(inputString) -> \(outputStrings) \n")
+
         }
         
         let errorRate = Double(iterations - successCount) / Double(iterations)
