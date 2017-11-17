ab9e08d3d2e24c7e26eaff164807056b8f797655
Xinyi Zhuang <yingmu52@msn.com>
2017-11-17 15:02:47 -0800

    516. Longest Palindromic Subsequence

---
+++ Leetcode/Go/longest_repeat_letter.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+// https://leetcode.com/problems/longest-palindromic-subsequence/description/
+
+func main() {
+	fmt.Println(longestPalindromeSubseq("bbbab"), 4)
+	fmt.Println(longestPalindromeSubseq("cbbd"), 2)
+	fmt.Println(longestPalindromeSubseq("aabaa"), 5)
+}
+
+func longestPalindromeSubseq(s string) int {
+	memo := map[string]int{} // index i, j : count
+	return lps(s, 0, len(s)-1, memo)
+}
+
+func lps(s string, i, j int, memo map[string]int) int {
+
+	// check memo
+	if val, ok := memo[string(i)+string(j)]; ok {
+		return val
+	}
+
+	if i == j { // case 1: only 1 char
+		return 1
+	}
+	if i+1 == j && s[i] == s[j] { // case 2: two char
+		return 2
+	}
+
+	result := 0
+	if s[i] == s[j] { // look at next inner string
+		result = lps(s, i+1, j-1, memo) + 2 // 2 means including s[i] and s[j]
+	} else {
+		result = max(lps(s, i+1, j, memo), lps(s, i, j-1, memo))
+	}
+	memo[string(i)+string(j)] = result
+	return result
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
